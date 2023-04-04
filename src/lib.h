6691881e4d77978153246337d50c3f3e14b7cd77
manyids2 <arun.mukundan@gmail.com>
2023-04-04 23:19:25 +0200

    Added OSMesa!

--- src/lib.h
+++ src/lib.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "../include/glad/glad.h"
+#include "GL/osmesa.h"
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
 #include "shader.h"
